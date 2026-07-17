76627276f464c20b0e0373ebdc1513cdb61acd42
SabinPant <sabinpant100@gmail.com>
2026-07-17 19:27:51 +0545

    feat: add CloudinaryConfig binding to AppServiceProvider

--- server/laravel/app/Providers/AppServiceProvider.php
+++ server/laravel/app/Providers/AppServiceProvider.php
@@ -1,7 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Providers;
 
+use App\DTOs\CloudinaryConfig;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +14,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(CloudinaryConfig::class, function () {
+            $config = config('services.cloudinary');
+
+            return new CloudinaryConfig(
+                cloudName: $config['cloud_name'],
+                apiKey:    $config['api_key'],
+                apiSecret: $config['api_secret'],
+            );
+        });
     }
 
     /**
