0285ba2090b5127d40085147a345c08cc7654525
SabinPant <sabinpant100@gmail.com>
2026-07-18 18:56:26 +0545

    fix: change personal_access_tokens migration to use uuidMorphs for UUID user IDs

--- server/laravel/app/Models/User.php
+++ server/laravel/app/Models/User.php
@@ -8,11 +8,12 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
 use App\Enums\UserRole;
 
 class User extends Authenticatable
 {
-    use HasFactory, HasUuids, SoftDeletes;
+    use HasApiTokens, HasFactory, HasUuids, SoftDeletes;
 
     /**
      * UUIDs are strings, not auto-incrementing integers.
