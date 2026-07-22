196123772733718fbe7424f63cb9f1ce059ddb7b
SabinPant <sabinpant100@gmail.com>
2026-07-22 15:14:12 +0545

    feat: add rate_limits section to skillswap config

--- server/laravel/config/skillswap.php
+++ server/laravel/config/skillswap.php
@@ -9,4 +9,43 @@ return [
     'avatar_max_size_kb'           => env('AVATAR_MAX_SIZE_KB', 2048),
     'chat_attachment_max_size_kb'  => env('CHAT_ATTACHMENT_MAX_SIZE_KB', 8192),
     'rating_cache_ttl_minutes'     => env('RATING_CACHE_TTL_MINUTES', 60),
+
+    'rate_limits' => [
+        'login' => [
+            'max_attempts'  => (int) env('RATE_LIMIT_LOGIN_MAX_ATTEMPTS', 5),
+            'decay_minutes' => (int) env('RATE_LIMIT_LOGIN_DECAY_MINUTES', 15),
+        ],
+        'register' => [
+            'max_attempts'  => (int) env('RATE_LIMIT_REGISTER_MAX_ATTEMPTS', 3),
+            'decay_minutes' => (int) env('RATE_LIMIT_REGISTER_DECAY_MINUTES', 60),
+        ],
+        'forgot_password' => [
+            'max_attempts'  => (int) env('RATE_LIMIT_FORGOT_PASSWORD_MAX_ATTEMPTS', 3),
+            'decay_minutes' => (int) env('RATE_LIMIT_FORGOT_PASSWORD_DECAY_MINUTES', 60),
+        ],
+        'resend_verification' => [
+            'max_attempts'  => (int) env('RATE_LIMIT_RESEND_VERIFICATION_MAX_ATTEMPTS', 3),
+            'decay_minutes' => (int) env('RATE_LIMIT_RESEND_VERIFICATION_DECAY_MINUTES', 60),
+        ],
+        'skill_request' => [
+            'max_attempts'  => (int) env('RATE_LIMIT_SKILL_REQUEST_MAX_ATTEMPTS', 10),
+            'decay_minutes' => (int) env('RATE_LIMIT_SKILL_REQUEST_DECAY_MINUTES', 1),
+        ],
+        'review' => [
+            'max_attempts'  => (int) env('RATE_LIMIT_REVIEW_MAX_ATTEMPTS', 10),
+            'decay_minutes' => (int) env('RATE_LIMIT_REVIEW_DECAY_MINUTES', 1),
+        ],
+        'chat_message' => [
+            'max_attempts'  => (int) env('RATE_LIMIT_CHAT_MESSAGE_MAX_ATTEMPTS', 30),
+            'decay_minutes' => (int) env('RATE_LIMIT_CHAT_MESSAGE_DECAY_MINUTES', 1),
+        ],
+        'avatar_upload' => [
+            'max_attempts'  => (int) env('RATE_LIMIT_AVATAR_UPLOAD_MAX_ATTEMPTS', 5),
+            'decay_minutes' => (int) env('RATE_LIMIT_AVATAR_UPLOAD_DECAY_MINUTES', 60),
+        ],
+        'default' => [
+            'max_attempts'  => (int) env('RATE_LIMIT_DEFAULT_MAX_ATTEMPTS', 100),
+            'decay_minutes' => (int) env('RATE_LIMIT_DEFAULT_DECAY_MINUTES', 1),
+        ],
+    ],
 ];
