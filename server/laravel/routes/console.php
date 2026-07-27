fc257e5de679682dc708cb73e589c81a19b64dcc
SabinPant <sabinpant100@gmail.com>
2026-07-27 21:11:37 +0545

    fix: add withoutOverlapping to expiry schedule for monitoring clarity

--- server/laravel/routes/console.php
+++ server/laravel/routes/console.php
@@ -4,4 +4,4 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::job(\App\Jobs\ExpireStaleSkillRequestsJob::class)->hourly();
+Schedule::job(\App\Jobs\ExpireStaleSkillRequestsJob::class)->hourly()->withoutOverlapping();
