1f8faa92a89c86b5078130f2c969078a06def855
SabinPant <sabinpant100@gmail.com>
2026-07-28 21:30:04 +0545

    fix: add ConversationFactory, run test migrations

---
+++ server/laravel/database/factories/ConversationFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\Conversation;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Conversation>
+ */
+class ConversationFactory extends Factory
+{
+    protected $model = Conversation::class;
+
+    public function definition(): array
+    {
+        return [
+            'user_one_id' => User::factory(),
+            'user_two_id' => User::factory(),
+        ];
+    }
+}
